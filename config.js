55c350a76056bd730adf9a0a36642d12be5dc753
SamwelJuma8 <161693993+SamwelJuma8@users.noreply.github.com>
2024-03-12 03:13:13 +0300

    Update sajun

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+global.owner = [SAJUN BOE
+  ['+255 759 350 402', 'SAJUN', true],
+  ['+255 758 988 031', 'BOE', true], 
   [''] 
 ] //Number of owners
 
@@ -50,7 +50,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'SAJUN BOE MD'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
